9b53a8ef44d4490b7a688a743c2d70f554165387
Gabrielfmontenegro <gabriel.franco@escolar.ifrn.edu.br>
2026-07-08 16:16:50 -0300

    :tada: init: Commit Inicial

---
+++ main.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
