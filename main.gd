aa0b224e02adb47aa387bd5b7790b73c40ccfd2c
Gabrielfmontenegro <gabriel.franco@escolar.ifrn.edu.br>
2026-07-13 14:24:46 -0300

    feat: foi adicionado mais uma cena, o HUD para o jogo

--- main.gd
+++ main.gd
@@ -4,29 +4,33 @@ extends Node
 var score
 
 func _ready():
-	new_game()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
-
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
+
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+	$HUD.update_score(score)
 
 
 func _on_mob_timer_timeout():
